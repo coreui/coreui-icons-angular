94c842657da5da854846c8ab5a85d4dd9adc4cce
xidedix <xidedix@users.noreply.github.com>
2021-11-18 22:35:32 +0100

    fix(icon-set.service): add get iconNames prop

--- projects/coreui-icons-angular/src/lib/icon-set/icon-set.service.ts
+++ projects/coreui-icons-angular/src/lib/icon-set/icon-set.service.ts
@@ -11,16 +11,21 @@ export class IconSetService {
 
   constructor() {}
 
-  // tslint:disable-next-line:variable-name
-  private _icons: IIconSet = {};
+  public get iconNames() {
+    return this._iconNames;
+  };
+  private _iconNames: { [key: string]: string } = {};
 
   get icons(): IIconSet {
     return this._icons;
   }
-
   set icons(iconSet) {
+    for (const iconsKey in iconSet) {
+      this._iconNames[iconsKey] = iconsKey;
+    }
     this._icons = iconSet;
   }
+  private _icons: IIconSet = {};
 
   public getIcon(name: string): string[] {
     const icon = this.icons[name];
